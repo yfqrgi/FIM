541540fadd41042a907982f7cf171e93bf6954b7
Yusufjon <qoraboyevyusufjon9@gmail.com>
2026-08-24 21:23:22 +0500

    Create baseline

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import hashlib
 import os
+import json
+import time
+import argparse
+
+BASELINE_FILE = 'baseline.json'
 
 def get_file_hash(filepath):
     hasher = hashlib.sha256()
@@ -11,6 +16,26 @@ def get_file_hash(filepath):
     except FileNotFoundError:
         print("File not found")
         return None
+
+def create_baseline(target_dir):
+    baseline = {}
+    print(f"[+] Creating Baseline for folder '{target_dir}'...")
+
+    for root, _, files in os.walk(target_dir):
+        for file in files:
+            if file == BASELINE_FILE:
+                continue
+
+            filepath = os.path.join(root, file)
+            file_hash = get_file_hash(filepath)
+            if file_hash:
+                baseline[filepath] = file_hash
+
+    with open(BASELINE_FILE, "w") as f:
+        json.dump(baseline, f, indent=4)
+
+    print(f"Baseline saved: {len(baseline)} files written to {BASELINE_FILE}")
+
 
 def check_integrity(filepath, original_hash):
     current_hash = get_file_hash(filepath)
@@ -28,6 +53,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(script_dir, "example.txt")
 original_hash = get_file_hash(filepath)
 
-if original_hash:
-    print(f"Initial hash {original_hash}")
-    check_integrity(filepath, original_hash)
+while True:
+    if original_hash:
+        print(f"Initial hash {original_hash}")
+        check_integrity(filepath, original_hash)
