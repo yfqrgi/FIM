c1036f99b71e10bfa648a602d0304ea800c7dc3d
Yusufjon <qoraboyevyusufjon9@gmail.com>
2026-08-24 21:11:17 +0500

    Check integrity, example.txt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 def get_file_hash(filepath):
     hasher = hashlib.sha256()
@@ -11,8 +12,22 @@ def get_file_hash(filepath):
         print("File not found")
         return None
 
-filepath = 'example.txt'
+def check_integrity(filepath, original_hash):
+    current_hash = get_file_hash(filepath)
+
+    print(f"Old hash {original_hash}")
+    print(f"Current hash {current_hash}")
+    print('-'*50)
+
+    if current_hash == original_hash:
+        print("FILE ENTIRE: No change detected")
+    else:
+        print('WARNING: The file has been modified or corrupted!')
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(script_dir, "example.txt")
 original_hash = get_file_hash(filepath)
 
 if original_hash:
     print(f"Initial hash {original_hash}")
+    check_integrity(filepath, original_hash)
